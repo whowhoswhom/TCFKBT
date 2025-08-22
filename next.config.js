29f2c0ca57c24794f0e61baee42c3c60677328b5
Jose Fuentes <143545948+whowhoswhom@users.noreply.github.com>
2025-08-22 14:36:40 -0400

    Use Next.js Image component

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-}
+  images: {
+    domains: ['via.placeholder.com'],
+  },
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
